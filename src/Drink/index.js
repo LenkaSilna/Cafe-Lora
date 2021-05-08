dddb2d38f56e355e6573de6dd868bfdbe6dbc808
Lenka Silna <lenka.baricova@post.cz>
2021-05-08 13:51:20 +0200

    Cafe Lora

---
+++ src/Drink/index.js
@@ -0,0 +1,45 @@
+import './style.css';
+import { Layer } from '../Layer';
+
+// komponenta pro layer
+export const Drink = (props) => {
+  const productElm = document.createElement('div');
+  productElm.classList.add('drink');
+  productElm.innerHTML = `
+  <div class="drink__product">
+    <div class="drink__cup">
+      <img src="/assets/cups/${props.id}.png" />
+    </div>
+    <div class="drink__info">
+      <h3>${props.name}</h3>
+    </div>
+  </div>
+  <div class="drink__controls">
+    <button class="order-btn">Objednat</button>
+  </div>`;
+
+  // zachytit element, kam vložím komponentu Layer
+  const productInfo = productElm.querySelector('.drink__info');
+
+  for (let i = 0; i < props.layers.length; i++) {
+    productInfo.appendChild(Layer(props.layers[i]));
+  }
+
+  // zachytit elementy pro objednávku
+  const orderButton = productElm.querySelector('.order-btn');
+  const drinkCup = productElm.querySelector('.drink__cup');
+
+  // na kliknutí vybrat kafe nebo zrušit objednávku
+  orderButton.addEventListener('click', () => {
+    if (props.ordered) {
+      orderButton.innerHTML = 'Objednat';
+      props.ordered = false;
+    } else {
+      orderButton.innerHTML = 'Zrušit';
+      props.ordered = true;
+    }
+    drinkCup.classList.toggle('drink__cup--selected');
+  });
+
+  return productElm;
+};
--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 import './style.css';
+import { Drink } from './Drink';
 
 console.log('funguju!');
+
+//=== MENU ===//
+
+// zachytit elementy
+const navbar = document.querySelector('#nav-btn');
+const navbarItems = document.querySelectorAll('.nav-item');
+const nav = document.querySelector('nav');
+
+// otevřít/zavřít menu přes button
+navbar.addEventListener('click', () => {
+  nav.classList.toggle('nav-closed');
+});
+
+// skrýt menu přes kliknutí na položku v menu
+for (let i = 0; i < navbarItems.length; i++) {
+  const navigation = navbarItems[i];
+  navigation.addEventListener('click', () => {
+    nav.classList.toggle('nav-closed');
+  });
+}
+
+//=== COFEE PRODUCTS ===//
+
+// zachytit element, kam vložím komponentu Drink
+
+const getDrinks = (drinks) => {
+  const product = document.querySelector('.drinks-list');
+  drinks.forEach((drink) => {
+    product.appendChild(Drink(drink));
+  });
+};
+
+fetch('https://apps.kodim.cz/daweb/cafelora/api/drinks')
+  .then((response) => response.json())
+  .then(getDrinks);

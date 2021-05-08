32ca306cb1adc3188daecc917031a4fe1e477153
Lenka Silna <lenka.baricova@post.cz>
2021-05-08 14:50:49 +0200

    ClassList layer

--- src/Layer/index.js
+++ src/Layer/index.js
@@ -3,7 +3,7 @@ import './style.css';
 // komponenta pro layer
 export const Layer = ({ color, label }) => {
   const layerElm = document.createElement('div');
-  layerElm.setAttribute('class', 'layer');
+  layerElm.classList.add('layer');
   layerElm.innerHTML = `
     <div class="layer__color" style="background-color: ${color}"></div>
     <div class="layer__label">${label}</div>`;
